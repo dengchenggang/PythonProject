e691f5e193c1c2c6ea1ac5c56bf169ca9ff01a9e
dengchenggang <dengchenggang.dcg@alibaba-inc.com>
2022-11-17 10:10:07 +0800

    【update】

--- main.py
+++ main.py
@@ -3,19 +3,17 @@
 import cv2
 import numpy as np
 
-#  wolrdPos:(-4.383876, -1.700000, 2.995136), uvPos:(171.545532, 305.912537, 1.000000) 
-# ARRenderer::OverlayCreate: type 0 
-#  wolrdPos:(-3.813700, -1.700000, 3.794065), uvPos:(255.660950, 304.677124, 1.000000) 
-# ARRenderer::OverlayCreate: type 0 
-#  wolrdPos:(-3.813700, -0.000000, 3.794065), uvPos:(276.038635, 633.218750, 1.000000) 
-# ARRenderer::OverlayCreate: type 0 
-#  wolrdPos:(-4.383876, 0.000000, 2.995136), uvPos:(242.404419, 605.591064, 1.000000) 
-
 if __name__ == "__main__":
     img = np.zeros((720, 1920, 3), np.uint8)
-    cv2.circle(img, (171, 305), 1, color=(0, 0, 255), thickness=2)
-    cv2.circle(img, (255, 304), 1, color=(0, 0, 255), thickness=2)
-    cv2.circle(img, (276, 633), 1, color=(0, 0, 255), thickness=2)
-    cv2.circle(img, (242, 605), 1, color=(0, 0, 255), thickness=2)
+    # cv2.circle(img, (171, 305), 1, color=(0, 0, 255), thickness=2)
+    # cv2.circle(img, (255, 304), 1, color=(0, 0, 255), thickness=2)
+    # cv2.circle(img, (276, 633), 1, color=(0, 0, 255), thickness=2)
+    # cv2.circle(img, (242, 605), 1, color=(0, 0, 255), thickness=2)
+
+    cv2.circle(img, (288, 299), 1, color=(0, 0, 255), thickness=2)
+    cv2.circle(img, (1767, 92), 1, color=(0, 0, 255), thickness=2)
+    cv2.circle(img, (310, 653), 1, color=(0, 0, 255), thickness=2)
+    cv2.circle(img, (1060, 367), 1, color=(0, 0, 255), thickness=2)
+
     cv2.imshow('img', img)
     cv2.waitKey(0)
